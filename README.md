d51682470dc99f0847105cb35d1f0aa5c5d71d79
Chris Zheng <z@caudate.me>
2016-09-03 15:06:25 +0800

    moved to tahto/wu.kong

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # wu.kong (悟空)
 
-[![Build Status](https://travis-ci.org/zcaudate/wu.kong.png?branch=master)](https://travis-ci.org/zcaudate/wu.kong)
+[![Build Status](https://travis-ci.org/tahto/wu.kong.png?branch=master)](https://travis-ci.org/tahto/wu.kong)
 [![Clojars](https://img.shields.io/clojars/v/im.chit/wu.kong.svg)](https://clojars.org/im.chit/wu.kong)
 
-![](https://raw.githubusercontent.com/zcaudate/wu.kong/master/monkey.jpg)
+![](https://raw.githubusercontent.com/tahto/wu.kong/master/monkey.jpg)
 
     “Nothing in this world is difficult, but thinking makes it
      seem so. Where there is true will, there is always a way.”
@@ -15,7 +15,7 @@
 Add to project.clj dependencies:
 
 ```clojure
-[im.chit/wu.kong "0.1.2"]
+[tahto/wu.kong "0.1.3"]
 ```
 
 ## Usage
--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject im.chit/wu.kong "0.1.2"
+(defproject tahto/wu.kong "0.1.4"
   :description "a wrapper for org.eclipse.aether"
-  :url "https://github.com/zcaudate/wu.kong"
+  :url "https://github.com/tahto/wu.kong"
   :license {:name "The MIT License"
             :url "http://http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
@@ -14,11 +14,9 @@
                  [org.eclipse.aether/aether-transport-file "1.1.0"]
                  [org.eclipse.aether/aether-transport-classpath "1.1.0"]
                  [org.apache.maven/maven-aether-provider "3.1.0"]
-                 [im.chit/hara.reflect "2.3.7"]
-                 [im.chit/hara.object "2.3.7"]
-                 [im.chit/hara.data.nested "2.3.7"]]
-  :profiles {:dev {:plugins [[lein-hydrox "0.1.16"]]
-                   :dependencies [;[im.chit/lucid "0.9.7"]
-                                  [midje "1.6.3"]
-                                  ;;[helpshift/hydrox "0.1.16"]
+                 [im.chit/hara.reflect "2.4.0"]
+                 [im.chit/hara.object "2.4.0"]
+                 [im.chit/hara.data.nested "2.4.0"]]
+  :profiles {:dev {:plugins [[lein-hydrox "0.1.17"]]
+                   :dependencies [[im.chit/hara.test "2.4.0"]
                                   ]}})
